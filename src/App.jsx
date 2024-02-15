654fbad128c565fb43827184a89287a34bc03b23
Akhil-Sharma-26 <2004akhil26sharma@gmail.com>
2024-02-15 23:39:40 +0530

    Init setup of React Router DOM

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,16 @@
 import { useState } from 'react'
-import { InputBox, Navbar } from './components'
+import { InputBox, Navbar, Header, Footer } from './components'
 import useCurrencyInfo from './hooks/UserCurrencyInfo.js'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Outlet, Route, Routes } from 'react-router-dom'
 import Currency_Converter from './Pages/Currency_Converter.jsx';
 import HomePage from './Pages/Home_Page.jsx';
 function App() {
     return (
         <>
-        <Navbar/>
-        <BrowserRouter>
-            <Routes>
-            <Route path="/currency-converter" element={<Currency_Converter />} />
-            <Route path='/' element={<HomePage/>}/>
-            </Routes>
-        </BrowserRouter>
+            <Header />
+                <Outlet />
+            <Footer />
         </>
-
     );
 }
 
--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,10 @@
 import InputBox from "./Input.jsx";
 import Navbar from "./Navbar.jsx";
+import Header from "./Header/Header.jsx";
+import Footer from "./Footer/Footer.jsx";
 export {
     InputBox,
-    Navbar
+    Navbar,
+    Header,
+    Footer
 }
